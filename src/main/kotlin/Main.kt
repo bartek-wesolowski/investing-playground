dd764167c87b375ff9d9e437a816f3eb4bc08ae4
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2024-10-11 20:52:50 +0200

    Improve printing results in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,48 @@ package com.bartoszwesolowski
 import com.bartoszwesolowski.model.SP500YearlyPriceProvider
 import com.bartoszwesolowski.scenario.BuyEveryYear
 import com.bartoszwesolowski.scenario.CompoundInvestmentScenario
+import com.bartoszwesolowski.scenario.InvestmentState
 import com.bartoszwesolowski.scenario.SellAfterTaxEveryYearUntilZero
 import com.bartoszwesolowski.strategy.SizedBucketInvestmentStrategy
 import com.bartoszwesolowski.strategy.LifoInvestmentStrategy
 import com.bartoszwesolowski.strategy.SimpleInvestmentStrategy
+import org.javamoney.moneta.format.CurrencyStyle
+import java.util.Locale
+import javax.money.format.AmountFormatQueryBuilder
+import javax.money.format.MonetaryAmountFormat
+import javax.money.format.MonetaryFormats
 
 fun main() {
     val verbose = false
     val priceProvider = SP500YearlyPriceProvider(100.usd)
     val tax = 0.19
+    val strategies = listOf(
+        SimpleInvestmentStrategy(verbose = verbose),
+        LifoInvestmentStrategy(verbose = verbose),
+        SizedBucketInvestmentStrategy(verbose = verbose, bucket = 500_000.usd)
+    )
     val scenario = CompoundInvestmentScenario(
         listOf(
             BuyEveryYear(100_000.usd, 8, priceProvider, tax),
             SellAfterTaxEveryYearUntilZero(100_000.usd, priceProvider, tax),
         )
     )
-    println(scenario.simulate(SimpleInvestmentStrategy(verbose = verbose)).last())
-    println(scenario.simulate(LifoInvestmentStrategy(verbose = verbose)).last())
-    println(scenario.simulate(SizedBucketInvestmentStrategy(verbose = verbose, bucket = 500_000.usd)).last())
+    for (strategy in strategies) {
+        println(strategy.javaClass.simpleName)
+        println(scenario.simulate(strategy).last().toPrettyString())
+    }
+}
+
+private val formatter: MonetaryAmountFormat = MonetaryFormats.getAmountFormat(
+    AmountFormatQueryBuilder.of(Locale.US)
+        .set(MonetaryFormats.getAmountFormat(Locale.US))
+        .set(CurrencyStyle.SYMBOL)
+        .build()
+)
+
+private fun InvestmentState.toPrettyString(): String {
+    return "  withdrawnValue=${formatter.format(withdrawnValue)},\n" +
+            "  withdrawnValueAfterTax=${formatter.format(withdrawnValueAfterTax)},\n" +
+            "  taxValue=${formatter.format(taxValue)}"
 }
 
